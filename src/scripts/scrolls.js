8be4858a9425aa1b14e688abcd9e7a9fb6a465f1
Roman <roman.pohribniak@gmail.com>
2021-08-01 19:12:43 +0300

    setup

---
+++ src/scripts/scrolls.js
@@ -0,0 +1,22 @@
+document.getElementById("banner-scroll").addEventListener("click", () => {
+  document
+    .getElementById("intro")
+    .scrollIntoView({ block: "center", behavior: "smooth" });
+});
+
+window.addEventListener("scroll", () => {
+  let scroll = (scrollTop =
+    window.pageYOffset || document.documentElement.scrollTop);
+
+  if (scroll > 150) {
+    document.getElementById("scroll-top").classList.remove("none");
+  } else {
+    document.getElementById("scroll-top").classList.add("none");
+  }
+});
+
+document.getElementById("scroll-top").addEventListener("click", () => {
+  document
+    .querySelector(".banner")
+    .scrollIntoView({ block: "center", behavior: "smooth" });
+});
